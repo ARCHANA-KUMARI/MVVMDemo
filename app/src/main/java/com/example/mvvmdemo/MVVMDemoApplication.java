99ed314342bda2c55542fe122a055780ec251067
Archana <ms.archanacusat2015@gmail.com>
2023-04-30 13:28:49 +0530

    First commit

---
+++ app/src/main/java/com/example/mvvmdemo/MVVMDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvvmdemo;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+/**
+ * @Author: Archana Kumari
+ * @Date: 30-04-2023
+ */
+@HiltAndroidApp
+public class MVVMDemoApplication extends Application {
+}
